ab9d9b0a6a1c8f619179b34477d5d4ad3df6d0ea
wana-dev <fluwanate1@gmail.com>
2020-11-23 20:22:01 +0900

    最初のコミット

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinutesGenerationApp.ViewModel
+{
+    /// <summary>
+    /// MV
+    /// </summary>
+    public class MainViewModel : BindableBase
+    {
+        public MainViewModel() 
+        {
+        }
+    }
+}
